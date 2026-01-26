ee5292566e93be1be0eb0327e5727970749a3928
ajaikumarMGA <ajaikumar.mga@gmail.com>
2026-01-26 19:41:39 +0530

    Dyanmic Services in Footer and Removed Services in Contact

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -99,7 +99,7 @@ const Contact = () => {
               </div>
               
               {/* Styling Services */}
-              <div className="bg-white p-8 flex-grow">
+              {/* <div className="bg-white p-8 flex-grow">
                 <h3 className="text-2xl font-bold text-gray-800 mb-4">Our Styling Services</h3>
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                   {[
@@ -118,7 +118,7 @@ const Contact = () => {
                     </div>
                   ))}
                 </div>
-              </div>
+              </div> */}
             </div>
           </div>
         </div>
